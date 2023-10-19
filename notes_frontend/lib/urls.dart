2f8a80c8ecd325a301013478e06226c235f8a60b
SalamiTech <salamigideon01@gmail.com>
2023-10-19 16:32:07 +0000

    .

--- notes_frontend/lib/urls.dart
+++ notes_frontend/lib/urls.dart
@@ -1,7 +1,11 @@
- class ApiUrls {
+class ApiUrls {
   static const String baseUrl = 'http://10.0.2.2:8000/';
+
+  // Routes
+  static const String getRoutes = baseUrl;
   static const String getNotes = "${baseUrl}notes/";
   static const String createNote = "${baseUrl}notes/create/";
   static String updateNote(String pk) => "${baseUrl}notes/$pk/update/";
   static String deleteNote(String pk) => "${baseUrl}notes/$pk/delete/";
+  static String getNote(String pk) => "${baseUrl}notes/$pk/";
 }
